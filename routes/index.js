e751a73910f2be915c52088941a1ee52c60ed53b
avu-ua <avuua@yahoo.com>
2024-09-27 18:10:04 +0200

    Seeding Events finalized

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const Event = require('../models/event')
 
+const { faker } = require('@faker-js/faker');
+
 // All Events Route
 router.get('/', async (req, res) => {
     try {
@@ -24,6 +26,7 @@ router.get('/seed-a-new', (req, res) => {
 router.post('/seed-a-new', async (req, res) => {
     const newEvents = populateEvents()
     try {
+        await Event.deleteMany({});
         for (const e of newEvents) {
             const event = new Event({
                 title: e.title,
@@ -41,24 +44,46 @@ router.post('/seed-a-new', async (req, res) => {
     }
 })
 
-function getRandomFutureDate() {
-    const today = new Date()
-    let futureDate = new Date(today)
-    const randomDays = Math.floor(Math.random() * 365)
-    futureDate.setDate(today.getDate() + randomDays)
-    return futureDate
+// Helper function generating a random event title
+const getEventTitle = () => {
+    const eventTypes = ['Conference', 'Seminar', 'Course', 'Presentation', 'Workshop', 'Lecture', 'Hackathon'];
+    const eventSubjects = ['JavaScript', 'NodeJS', 'Cyber Security', 'Python', 'Design Patterns', 'Kotlin', 'Computer Science']
+    const eventTitle = [eventTypes[Math.floor(Math.random() * eventTypes.length)], 
+                        eventSubjects[Math.floor(Math.random() * eventSubjects.length)]]
+    return eventTitle.join(' on ')
+};
+
+// Helper function generating a random number of up to 15 dummy participants
+function generateParticipants() {
+    const participantsNumber = Math.floor(Math.random() * 15)
+    const participants = []
+    for (let i = 0; i < participantsNumber; i++) {
+        const firstName = faker.person.firstName()
+        const source = ['Social media', 'Friends', 'Found myself']
+        const participant = {
+            fullName: faker.person.fullName({firstName: firstName}),
+            email: faker.internet.email({firstName: firstName}),
+            birthDate: faker.date.birthdate(),
+            source: source[Math.floor(Math.random() * source.length)]
+        }
+        participants.push(participant)
+    }
+    return participants
 }
 
+// Helper function generating random number of dummy Events
 function populateEvents() {
+    const eventsNumber = Math.floor(Math.random() * 51) + 50
     const events = []
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < eventsNumber; i++) {
         const event = {
-            title: `Title ${i + 1}`,
-            description: `Description ${i + 1}`,
-            date: getRandomFutureDate(),
-            organizer: `Organizer ${i + 1}`,
-            participants: []
+            title: getEventTitle(),
+            // description: faker.lorem.lines({ min: 1, max: 2 }),
+            description: faker.lorem.paragraph(),
+            date: faker.date.future(),
+            organizer: faker.company.name(),
+            participants: generateParticipants()
         }
         events.push(event)
     }
